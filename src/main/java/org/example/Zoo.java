362d41e14a18c796f44358b8824cbfa1e66d1ae3
najmahal25 <janryan.hidalgo@gcash.com>
2025-07-11 13:49:12 +0800

    added arraylist of people, animals, and buildings and corresponding setter and getter methods

--- src/main/java/org/example/Zoo.java
+++ src/main/java/org/example/Zoo.java
@@ -1,13 +1,42 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Zoo {
     private boolean isOpen;
-    private
+    private ArrayList<People> listOfPeople;
+    private ArrayList<Animal> listOfAnimals;
+    private ArrayList<Building> listOfBuildings;
+
     public boolean isOpen() {
         return isOpen;
     }
 
     public void setOpen(boolean open) {
         isOpen = open;
+    }
+
+    public ArrayList<People> getListOfPeople() {
+        return listOfPeople;
+    }
+
+    public void setListOfPeople(ArrayList<People> listOfPeople) {
+        this.listOfPeople = listOfPeople;
+    }
+
+    public ArrayList<Animal> getListOfAnimals() {
+        return listOfAnimals;
+    }
+
+    public void setListOfAnimals(ArrayList<Animal> listOfAnimals) {
+        this.listOfAnimals = listOfAnimals;
+    }
+
+    public ArrayList<Building> getListOfBuildings() {
+        return listOfBuildings;
+    }
+
+    public void setListOfBuildings(ArrayList<Building> listOfBuildings) {
+        this.listOfBuildings = listOfBuildings;
     }
 }
