5d341190bc9b161c4d7320f5e4d9c4ce9d546839
ericconradpanga <ericconrad.pangaiii@gcash.com>
2025-07-11 12:18:24 +0800

    added Ticket class

---
+++ src/main/java/org/example/Ticket.java
@@ -0,0 +1,57 @@
+package org.example;
+
+public class Ticket {
+    private String name;
+    private int age;
+    private String type;
+    private double price;
+    private String code;
+
+    public Ticket(String name, int age, String type, double price, String code) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.price = price;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
